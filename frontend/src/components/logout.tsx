015cee58670b4b2f2be8be2a4839caa81a89805a
mehak-think41 <mehak.khanna@think41.com>
2025-03-19 12:13:09 +0530

    rm: frontend dir

--- frontend/src/components/logout.tsx
+++
@@ -1,9 +0,0 @@
-import { useLogout } from "../context/AuthContext";
-
-const LogoutButton = () => {
-    const logout = useLogout();
-
-    return <button onClick={logout}>Logout</button>;
-};
-
-export default LogoutButton;
